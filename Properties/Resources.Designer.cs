bd8168c69bdb1664b3d24e17b8147cbff52792f4
jason.cavett <jason.cavett@3790a856-0193-11df-b136-7f7962f7bc17>
2010-02-14 21:11:00 +0000

    Added a search button to browse to a folder that contains a software project (done in the NewProjectDialog).

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -115,5 +115,12 @@ namespace CAE.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        internal static System.Drawing.Bitmap search {
+            get {
+                object obj = ResourceManager.GetObject("search", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
