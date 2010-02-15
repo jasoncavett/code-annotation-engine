f03a7369f03efa7c3063a982f2aca23c248e9e41
jason.cavett <jason.cavett@3790a856-0193-11df-b136-7f7962f7bc17>
2010-02-15 02:53:21 +0000

    Added the FSTree.  Made updates to display the project (not yet finished).

--- src/gui/NewProjectDialog.Designer.cs
+++ src/gui/NewProjectDialog.Designer.cs
@@ -87,6 +87,7 @@
             this.okButton.TabIndex = 4;
             this.okButton.Text = "OK";
             this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
             // 
             // cancelButton
             // 
