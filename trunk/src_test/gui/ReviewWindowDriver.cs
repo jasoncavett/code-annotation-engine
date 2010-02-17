5fce205f7014028df570282e7a5913a7ee8d4d87
jason.cavett <jason.cavett@3790a856-0193-11df-b136-7f7962f7bc17>
2010-02-17 01:30:55 +0000

    Added src_test documents.

---
+++ trunk/src_test/gui/ReviewWindowDriver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using CAE.src.gui;
+
+namespace CAE.src_test.gui
+{
+    class ReviewWindowDriver
+    {
+        /// <summary>
+        /// The main entry point for the ReviewWindow.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ReviewWindow());
+        }
+    }
+}
