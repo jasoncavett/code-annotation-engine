a83e7b5b325968a386b08cfb9af3219dbaf8db45
iceman3897@aol.com <iceman3897@aol.com@3790a856-0193-11df-b136-7f7962f7bc17>
2010-02-18 03:21:30 +0000

    Added the syntaxDocument1 to the ReviewWindow

--- CAE/src/gui/ReviewWindow.Designer.cs
+++ CAE/src/gui/ReviewWindow.Designer.cs
@@ -28,13 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.syntaxBoxControl1 = new Alsing.Windows.Forms.SyntaxBoxControl();
+            this.syntaxDocument1 = new Alsing.SourceCode.SyntaxDocument(this.components);
             this.SuspendLayout();
+            // 
+            // syntaxBoxControl1
+            // 
+            this.syntaxBoxControl1.ActiveView = Alsing.Windows.Forms.ActiveView.BottomRight;
+            this.syntaxBoxControl1.AutoListPosition = null;
+            this.syntaxBoxControl1.AutoListSelectedText = "";
+            this.syntaxBoxControl1.AutoListVisible = false;
+            this.syntaxBoxControl1.BackColor = System.Drawing.Color.White;
+            this.syntaxBoxControl1.BorderStyle = Alsing.Windows.Forms.BorderStyle.None;
+            this.syntaxBoxControl1.CopyAsRTF = false;
+            this.syntaxBoxControl1.Document = this.syntaxDocument1;
+            this.syntaxBoxControl1.FontName = "Courier new";
+            this.syntaxBoxControl1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.syntaxBoxControl1.InfoTipCount = 1;
+            this.syntaxBoxControl1.InfoTipPosition = null;
+            this.syntaxBoxControl1.InfoTipSelectedIndex = 0;
+            this.syntaxBoxControl1.InfoTipVisible = false;
+            this.syntaxBoxControl1.Location = new System.Drawing.Point(89, 47);
+            this.syntaxBoxControl1.LockCursorUpdate = false;
+            this.syntaxBoxControl1.Name = "syntaxBoxControl1";
+            this.syntaxBoxControl1.ShowScopeIndicator = false;
+            this.syntaxBoxControl1.Size = new System.Drawing.Size(383, 218);
+            this.syntaxBoxControl1.SmoothScroll = false;
+            this.syntaxBoxControl1.SplitviewH = -4;
+            this.syntaxBoxControl1.SplitviewV = -4;
+            this.syntaxBoxControl1.TabGuideColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(231)))), ((int)(((byte)(235)))));
+            this.syntaxBoxControl1.TabIndex = 0;
+            this.syntaxBoxControl1.Text = "syntaxBoxControl1";
+            this.syntaxBoxControl1.WhitespaceColor = System.Drawing.SystemColors.ControlDark;
+            // 
+            // syntaxDocument1
+            // 
+            this.syntaxDocument1.Lines = new string[] {
+        ""};
+            this.syntaxDocument1.MaxUndoBufferSize = 1000;
+            this.syntaxDocument1.Modified = false;
+            this.syntaxDocument1.UndoStep = 0;
             // 
             // ReviewWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(608, 300);
+            this.Controls.Add(this.syntaxBoxControl1);
             this.Name = "ReviewWindow";
             this.Text = "ReviewWindow";
             this.ResumeLayout(false);
@@ -42,6 +83,9 @@
         }
 
         #endregion
+
+        private Alsing.Windows.Forms.SyntaxBoxControl syntaxBoxControl1;
+        private Alsing.SourceCode.SyntaxDocument syntaxDocument1;
 
     }
 }
