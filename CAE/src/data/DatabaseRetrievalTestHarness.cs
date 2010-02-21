3f4d5afa2cf9e8abd6c649c625a8e6ddd6dce84a
joelmanon.jm <joelmanon.jm@3790a856-0193-11df-b136-7f7962f7bc17>
2010-02-21 22:05:20 +0000

    Added data/DatabaseRetrievalTestHarness which successfully executes a stored procedure.  Had to code a dummy ReviewWindowDriver, because the project would not build without it.

---
+++ CAE/src/data/DatabaseRetrievalTestHarness.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CAE.src_test.data
+{
+    class DatabaseRetrievalTestHarness
+    {
+        public static void Main()
+        {
+            SqlConnection mySqlConnection = new SqlConnection("server=(local)\\SQLEXPRESS;database=CAE;Integrated Security=SSPI;");
+
+            SqlCommand mySqlCommand = mySqlConnection.CreateCommand();
+            mySqlCommand.CommandText = "EXECUTE dbo.list_mods @project_nm";
+            mySqlCommand.Parameters.Add("@project_nm", SqlDbType.VarChar, 20).Value = "order_mgt";
+            SqlDataAdapter mySqlDataAdapter = new SqlDataAdapter();
+            mySqlDataAdapter.SelectCommand = mySqlCommand;
+            mySqlConnection.Open();
+            DataSet myDataSet = new DataSet();
+            Console.WriteLine("Retrieving rows from the List Modules Procedure");
+            int numberOfRows = mySqlDataAdapter.Fill(myDataSet, "list_mods");
+            Console.WriteLine("numberOfRows = " + numberOfRows);
+            mySqlConnection.Close();
+            DataTable myDataTable = myDataSet.Tables["list_mods"];
+            foreach (DataRow myDataRow in myDataTable.Rows)
+            {
+                Console.WriteLine("ProjectName = " + myDataRow["project_nm"]);
+                Console.WriteLine("ModuleName = " + myDataRow["module_nm"]);
+                Console.WriteLine("ModuleDesc = " + myDataRow["module_desc"]);
+                Console.WriteLine("Lang = " + myDataRow["lang"]);
+            }
+        }
+ 
+    }
+}
---
+++ CAE/src_test/gui/ReviewWindowDriver.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAE.src_test.gui
+{
+    class ReviewWindowDriver
+    {
+    public static void dummy()
+        {
+        }
+    }
+}
