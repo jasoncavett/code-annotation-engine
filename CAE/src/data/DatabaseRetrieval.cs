93e9d9c23bf1765cbe040fc0cf2e70f47d221b84
joelmanon.jm <joelmanon.jm@3790a856-0193-11df-b136-7f7962f7bc17>
2010-02-21 20:14:47 +0000

    Added DatabaseRetrieval class with ListModules Method

---
+++ CAE/src/data/DatabaseRetrieval.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CAE.src.data
+{
+    class DatabaseRetrieval
+    {
+    public static void ListModules(string project_nm)
+        {
+        SqlConnection mySqlConnection =new SqlConnection(CAE.Properties.Settings.CAEConnectionString);
+
+        SqlCommand mySqlCommand = mySqlConnection.CreateCommand();
+        mySqlCommand.CommandText = "EXECUTE dbo.list_mods @project_nm";
+        mySqlCommand.Parameters.Add("@project_nm", SqlDbType.VarChar, 20).Value = project_nm;
+
+        SqlDataAdapter mySqlDataAdapter = new SqlDataAdapter();
+        mySqlDataAdapter.SelectCommand = mySqlCommand;
+        DataSet myDataSet = new DataSet();
+        mySqlConnection.Open();
+        int numberOfRows = mySqlDataAdapter.Fill(myDataSet, "list_mods");
+        mySqlConnection.Close();
+        }
+    }
+}
