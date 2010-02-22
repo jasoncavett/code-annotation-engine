0b5a21d0e4d8f2eb0591f748dbe78c79e639f62b
joelmanon.jm <joelmanon.jm@3790a856-0193-11df-b136-7f7962f7bc17>
2010-02-22 03:42:31 +0000

    Improved DB Retrieval methods to use parameters instead of hard-coded values.  Updated Test Harnesses to pass parameters.  Added a new method and test harness for List Module Revisions.

---
+++ CAE/src/data/DatabaseRetrievalTestHarnessListMods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CAE.src.data
+{
+    public class DatabaseRetrievalTestHarnessListMods
+    {
+        public static void Main()
+        {
+            int numberOfRows = 0;
+            string project_nm = "order_mgt";
+            DataSet myDataSet = new DataSet();
+            DatabaseReader.ListModules(project_nm, myDataSet);
+            Console.WriteLine("Retrieving rows from the List Modules Procedure");
+            Console.WriteLine("numberOfRows = " + numberOfRows);
+            DataTable myDataTable = myDataSet.Tables["list_mods"];
+            foreach (DataRow myDataRow in myDataTable.Rows)
+            {
+                Console.WriteLine("ProjectName = " + myDataRow["project_nm"]);
+                Console.WriteLine("ModuleName = " + myDataRow["module_nm"]);
+                Console.WriteLine("ModuleDesc = " + myDataRow["module_desc"]);
+                Console.WriteLine("Lang = " + myDataRow["lang"]);
+                Console.WriteLine("AuthorLastName = " + myDataRow["author_last_nm"]);
+                Console.WriteLine("AuthorFirstName = " + myDataRow["author_first_nm"]);
+            }
+        }
+    }
+}
