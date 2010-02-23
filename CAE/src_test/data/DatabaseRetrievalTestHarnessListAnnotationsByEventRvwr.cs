0f186679d9271d143617b9112f1ddd5e62176dfc
joelmanon.jm <joelmanon.jm@3790a856-0193-11df-b136-7f7962f7bc17>
2010-02-23 05:24:36 +0000

    Added remaining 2 DatabaseReader methods and Test Harnesses for them.

---
+++ CAE/src_test/data/DatabaseRetrievalTestHarnessListAnnotationsByEventRvwr.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CAE.src.data
+{
+    public class DatabaseRetrievalTestHarnessListAnnotationsByEventRvwr
+    {
+        public static void Main()
+        {
+// declare variables needed by DatabaseReader method ListAnnotations
+// these would normally be variables, not literals:
+            string project_nm = "order_mgt";
+            string module_nm = "java_main";
+            decimal revision_no = 1.1M;
+            string rvw_event_dt = "02/08/2010";
+            string rvwr_last_nm = "Rosa";
+            string rvwr_first_nm = "Juan";
+            DataSet myDataSet = new DataSet();
+// call DatabaseReader method ListAnnotations to return a list of all annotations for a given review
+// of a given revision of a module in a project by a given reviewer:
+            DatabaseReader.ListAnnotations(project_nm, module_nm, revision_no, rvw_event_dt, rvwr_last_nm, rvwr_first_nm, myDataSet);
+            Console.WriteLine("Retrieving rows from the ListAnnotations Procedure");
+// result set returned from Stored Procedure ends up in the DataSet's DataTable:
+            DataTable myDataTable = myDataSet.Tables["list_annotations_by_evnt_rvwr"];
+// loop through DataRows of the DataTable pulling off the fields you need
+// by name within square brackets:
+            foreach (DataRow myDataRow in myDataTable.Rows)
+            {
+                Console.WriteLine("ProjectName = " + myDataRow["project_nm"]);
+                Console.WriteLine("ModuleName = " + myDataRow["module_nm"]);
+                Console.WriteLine("ModuleDesc = " + myDataRow["module_desc"]);
+                Console.WriteLine("Lang = " + myDataRow["lang"]);
+                Console.WriteLine("AuthorLastName = " + myDataRow["author_last_nm"]);
+                Console.WriteLine("AuthorFirstName = " + myDataRow["author_first_nm"]);
+                Console.WriteLine("RevisionNo = " + myDataRow["revision_no"]);
+                Console.WriteLine("ChangeDesc = " + myDataRow["chg_desc"]);
+                Console.WriteLine("Developer Last Name = " + myDataRow["devlpr_last_nm"]);
+                Console.WriteLine("Developer First Name = " + myDataRow["devlpr_first_nm"]);
+                Console.WriteLine("Review Event Date = " + myDataRow["rvw_event_dt"]);
+                Console.WriteLine("Review Event Desc = " + myDataRow["rvw_event_desc"]);
+                Console.WriteLine("Reviewer Last Name = " + myDataRow["rvwr_last_nm"]);
+                Console.WriteLine("Reviewer First Name = " + myDataRow["rvwr_first_nm"]);
+                Console.WriteLine("Annotation Text = " + myDataRow["annotation_txt"]);
+            }
+        }
+    }
+}
