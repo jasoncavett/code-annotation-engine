b4885bfb02f07f525ae0b0959c61e5c6910d780c
jason.cavett <jason.cavett@3790a856-0193-11df-b136-7f7962f7bc17>
2010-03-03 00:58:26 +0000

    Switched to making ProjectView a panel.

--- CAE/src/gui/ProjectView.cs
+++ CAE/src/gui/ProjectView.cs
@@ -1,18 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
 namespace CAE.src.gui
 {
-    public partial class ProjectView : Form
+    public partial class ProjectView : Panel
     {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         public ProjectView()
         {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Initializing constructor.
+        /// </summary>
+        /// <param name="container">The container to add this view to.</param>
+        public ProjectView(IContainer container)
+        {
+            container.Add(this);
+
             InitializeComponent();
         }
     }
