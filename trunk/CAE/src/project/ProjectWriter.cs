3d7c7cd586cbd91450e11b8fcde91e2ec2ac131d
jason.cavett <jason.cavett@3790a856-0193-11df-b136-7f7962f7bc17>
2010-03-06 18:34:06 +0000

    Created the ProjectWriter which will create the project files.

--- trunk/CAE/src/project/ProjectWriter.cs
+++ trunk/CAE/src/project/ProjectWriter.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Xml;
 
 namespace CAE.src.project
 {
     public static class ProjectWriter
     {
+        private const string PROJECT_FILE_NAME = "project.cae";
+
         /// <summary>
-        /// Read a project file from a local path.
+        /// Read a project file from a local path.  If the path is not available,
+        /// a null project will be returned.
         /// </summary>
         /// <param name="path">The path to the project file.</param>
         /// <returns>The project built from the project file.</returns>
@@ -20,6 +24,21 @@ namespace CAE.src.project
             // Parse the file and build the project.
             if (File.Exists(path))
             {
+                XmlTextReader textReader = new XmlTextReader(path);
+                textReader.Read();
+
+                while (textReader.Read())
+                {
+                    switch (textReader.Name)
+                    {
+                        case "project-name":
+                            project.Title = textReader.Value;
+                            break;
+                        case "svn-path":
+                            project.SvnPath = textReader.Value;
+                            break;
+                    }
+                }
             }
 
             return project;
@@ -30,9 +49,27 @@ namespace CAE.src.project
         /// local path inside the project itself.
         /// </summary>
         /// <param name="project">The project to write.</param>
-        public static void WriteProject(Project project)
+        /// <param name="path">The path that the project file will be written.</param>
+        public static void WriteProject(Project project, string path)
         {
+            if (Directory.Exists(path))
+            {
+                FileStream fs = new FileStream(path + ProjectWriter.PROJECT_FILE_NAME, FileMode.Create);
 
+                XmlWriter w = XmlWriter.Create(fs);
+
+                w.WriteStartDocument();
+                w.WriteStartElement("project");
+
+                w.WriteElementString("project-name", project.Title);
+                w.WriteElementString("svn-path", project.SvnPath);
+                
+                w.WriteEndElement();
+                w.WriteEndDocument();
+                w.Flush();
+
+                fs.Close();
+            }
         }
     }
 }
