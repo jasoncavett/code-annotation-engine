2e57bf3d88c95fa150bb888e79195ec4eed47ec9
jason.cavett <jason.cavett@3790a856-0193-11df-b136-7f7962f7bc17>
2010-03-09 04:22:55 +0000

    Added Java project files.

---
+++ test/src/test/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * The driver for the application.
+ * 
+ * @author Jason Cavett
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
