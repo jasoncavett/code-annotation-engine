7d8b42a24e845c6ef2d0b47f377e83624082ac0f
jason.cavett <jason.cavett@3790a856-0193-11df-b136-7f7962f7bc17>
2010-03-11 04:38:41 +0000

    Added the AnnotationDialog which will allow the user to enter the annotation information.

---
+++ CAE/src/gui/AnnotationDialog.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace CAE.src.gui
+{
+    partial class AnnotationDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // AnnotationDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 165);
+            this.Name = "AnnotationDialog";
+            this.Text = "AnnotationDialog";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
