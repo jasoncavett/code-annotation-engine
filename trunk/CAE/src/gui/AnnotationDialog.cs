993f4ae138faf5a6fe6a477d58f43848cbebb6f0
jason.cavett <jason.cavett@3790a856-0193-11df-b136-7f7962f7bc17>
2010-03-11 04:38:41 +0000

    Added the AnnotationDialog which will allow the user to enter the annotation information.

---
+++ trunk/CAE/src/gui/AnnotationDialog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CAE.src.gui
+{
+    public partial class AnnotationDialog : Form
+    {
+        public AnnotationDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
