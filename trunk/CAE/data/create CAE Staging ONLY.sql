c0853bce2f1ebabbdbd92f829403feba08bcbc3e
joelmanon.jm <joelmanon.jm@3790a856-0193-11df-b136-7f7962f7bc17>
2010-03-28 04:03:47 +0000

    Added ImportAnnotations method to the DatabaseWriter class and the ExportAnnotations method to the DatabaseReader class, along with 2 Test Harness classes (ImportTestHarness and ExportTestHarness).
    Added 3 new SQL scripts that need to be run and executed in the database: "create CAE Staging ONLY.sql", which creates the new staging table, and 2 SPs: "create trunc_staging.sql" and "create merge_annotations.sql"

---
+++ trunk/CAE/data/create CAE Staging ONLY.sql
@@ -0,0 +1,42 @@
+/*
+ * ER/Studio 7.5 SQL Code Generation
+ * Company :      Public
+ * Project :      Practice.dm1
+ * Author :       Joel 
+ *
+ * Date Created : Saturday, March 27, 2010 14:23:25
+ * Target DBMS : Microsoft SQL Server 2005
+ */
+
+USE CAE
+go
+
+IF OBJECT_ID('Import_Stage') IS NOT NULL
+BEGIN
+    DROP TABLE Import_Stage
+    PRINT '<<< DROPPED TABLE Import_Stage >>>'
+END
+go
+
+/* 
+ * TABLE: Import_Stage 
+ */
+
+CREATE TABLE Import_Stage(
+    project_nm          varchar(20)      NOT NULL,
+    codefile_nm         varchar(50)      NOT NULL,
+    codefile_line_no    int              NOT NULL,
+    rvwr_last_nm        varchar(20)      NOT NULL,
+    rvwr_first_nm       varchar(20)      NOT NULL,
+    annotation_txt      varchar(2000)    NOT NULL,
+    last_update_dtm     datetime         NOT NULL,
+    CONSTRAINT PK8 PRIMARY KEY NONCLUSTERED (project_nm, codefile_nm, codefile_line_no, rvwr_last_nm, rvwr_first_nm)
+)
+go
+
+IF OBJECT_ID('Import_Stage') IS NOT NULL
+    PRINT '<<< CREATED TABLE Import_Stage >>>'
+ELSE
+    PRINT '<<< FAILED CREATING TABLE Import_Stage >>>'
+go
+
---
+++ trunk/CAE/src_test/data/ImportTestHarness.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using CAE.src.data;
+
+namespace CAE.src_test.data
+{
+    public class ImportTestHarness
+    {
+        public static void Main()
+        {
+            // declare variables needed by DatabaseReader method ImportAnnotations
+            // these would normally be variables, not literals:
+            string ImportFile = @"C:\SWENG500\Export.txt";
+            string FormatFile = @"C:\SWENG500\ReviewAnnotationFormat.txt";
+            string rvwr_last_nm = "Manon";
+            string rvwr_first_nm = "Joel";
+
+            // call DatabaseWriter method ImportAnnotations to import annotations into database
+            StringBuilder errorMessages =
+                DatabaseWriter.ImportAnnotations(ImportFile, FormatFile, rvwr_last_nm, rvwr_first_nm);
+            Console.WriteLine("Importing data via Import Annotations");
+            Console.WriteLine(errorMessages.ToString());
+        }
+    }
+}
