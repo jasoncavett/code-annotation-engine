bf892d8593dfccfd6e4637d8c4ba8ca76d9719c6
jason.cavett <jason.cavett@3790a856-0193-11df-b136-7f7962f7bc17>
2010-04-03 19:06:15 +0000

    Updated to give different reviewers different color markers.

--- CAE/src/gui/ProjectView.cs
+++ CAE/src/gui/ProjectView.cs
@@ -18,6 +18,39 @@ namespace CAE.src.gui
     {
         public Project Project { get; set; }
 
+        private Dictionary<string, Color> UserAnnotations;
+        private double hue;
+
+        /// <summary>
+        /// Represents RGB color system.
+        /// 
+        /// From: http://www.geekymonkey.com/Programming/CSharp/RGB2HSL_HSL2RGB.htm
+        /// </summary>
+        public struct ColorRGB
+        {
+            public byte R;
+            public byte G;
+            public byte B;
+
+            public ColorRGB(Color value)
+            {
+                this.R = value.R;
+                this.G = value.G;
+                this.B = value.B;
+            }
+
+            public static implicit operator Color(ColorRGB rgb)
+            {
+                Color c = Color.FromArgb(rgb.R, rgb.G, rgb.B);
+                return c;
+            }
+
+            public static explicit operator ColorRGB(Color c)
+            {
+                return new ColorRGB(c);
+            }
+        }
+
         /// <summary>
         /// Initialization constructor.
         /// </summary>
@@ -25,6 +58,8 @@ namespace CAE.src.gui
         public ProjectView(Project project)
         {
             this.Project = project;
+            this.UserAnnotations = new Dictionary<string, Color>();
+            this.hue = 0.0;
 
             InitializeComponent();
             SpecialInitializations();
@@ -40,7 +75,19 @@ namespace CAE.src.gui
         /// <param name="reviewerName">The name of the reviewer.</param>
         private void AddLineMarker(int lineNo, string reviewerName)
         {
-            scintilla1.Lines[lineNo].AddMarker(1);
+            Marker marker = scintilla1.Markers[1];
+
+            // Generate the next color if the user annotations does
+            // not already exist in the document.
+            if (!UserAnnotations.ContainsKey(reviewerName))
+            {
+                hue += 0.03 % 1;
+                ColorRGB c = HSL2RGB(hue, 0.5, 0.5);
+                UserAnnotations[reviewerName] = c;
+            }
+
+            marker.BackColor = UserAnnotations[reviewerName];
+            scintilla1.Lines[lineNo].AddMarker(marker);
         }
 
         /// <summary>
@@ -51,6 +98,78 @@ namespace CAE.src.gui
         {
             scintilla1.AppendText(line);
             scintilla1.AppendText(Environment.NewLine);
+        }
+
+        /// <summary>
+        /// Given H,S,L in range of 0-1, returns a Color (RGB struct) in range of 0-255.
+        /// </summary>
+        /// <param name="h"></param>
+        /// <param name="sl"></param>
+        /// <param name="l"></param>
+        /// <returns></returns>
+        public static ColorRGB HSL2RGB(double h, double sl, double l)
+        {
+            double v;
+            double r, g, b;
+
+            r = l;   // default to gray
+            g = l;
+            b = l;
+            v = (l <= 0.5) ? (l * (1.0 + sl)) : (l + sl - l * sl);
+            if (v > 0)
+            {
+                double m;
+                double sv;
+                int sextant;
+                double fract, vsf, mid1, mid2;
+
+                m = l + l - v;
+                sv = (v - m) / v;
+                h *= 6.0;
+                sextant = (int)h;
+                fract = h - sextant;
+                vsf = v * sv * fract;
+                mid1 = m + vsf;
+                mid2 = v - vsf;
+                switch (sextant)
+                {
+                    case 0:
+                        r = v;
+                        g = mid1;
+                        b = m;
+                        break;
+                    case 1:
+                        r = mid2;
+                        g = v;
+                        b = m;
+                        break;
+                    case 2:
+                        r = m;
+                        g = v;
+                        b = mid1;
+                        break;
+                    case 3:
+                        r = m;
+                        g = mid2;
+                        b = v;
+                        break;
+                    case 4:
+                        r = mid1;
+                        g = m;
+                        b = v;
+                        break;
+                    case 5:
+                        r = v;
+                        g = m;
+                        b = mid2;
+                        break;
+                }
+            }
+            ColorRGB rgb;
+            rgb.R = Convert.ToByte(r * 255.0f);
+            rgb.G = Convert.ToByte(g * 255.0f);
+            rgb.B = Convert.ToByte(b * 255.0f);
+            return rgb;
         }
 
         /// <summary>
